362dbe5af7e3e7da763114472092b7df3b9f45f4
gm_kafi <kaficsebaiust02@gmail.com>
2023-05-04 22:01:35 +0600

    Accomplish ui done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:feedapp/ui/screens/accomplish_screen.dart';
 import 'package:feedapp/ui/screens/register_screen.dart';
 import 'package:feedapp/ui/screens/varification_screen.dart';
 import 'package:flutter/material.dart';
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: VarificationScreen(),
+      home: AccomplishScreen(),
     );
   }
 }
---
+++ lib/ui/screens/accomplish_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class AccomplishScreen extends StatelessWidget {
+  const AccomplishScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        leading: const Icon(Icons.home),
+        title: const Text("Accomplish"),
+        centerTitle: true,
+        actions:  [
+          Image.asset("assets/logo.PNG",scale: 5,),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Image.asset("assets/success_message.PNG"),
+            ),
+            SizedBox(height: 60,),
+            InkWell(
+                onTap: () {},
+                child: const Text(
+                  "Login to your account",
+                  style: TextStyle(color: Colors.blue,fontSize: 24),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
